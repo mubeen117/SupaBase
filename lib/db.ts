05fbb2ce4c1780c2bffc0d9cbd39d46b31bf7b85
saddam <62154427+saddamlakho@users.noreply.github.com>
2024-12-10 11:10:48 +0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -42,9 +42,10 @@
 //   }
 //   return client;
 // };
+import { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from 'pg';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const client = new Client({
     connectionString: process.env.POSTGRES_URL, // Ensure the environment variable is correctly set
     ssl: {
@@ -53,12 +54,16 @@ export default async function handler(req, res) {
   });
 
   try {
+    // Establishing connection to PostgreSQL database
     await client.connect();
     res.status(200).json({ message: 'Successfully connected to the database' });
-  } catch (error) {
+  } catch (error: any) {
+    // Handling error in case of connection failure
     console.error('Connection error:', error.message);
     res.status(500).json({ error: 'Failed to connect to the database', details: error.message });
   } finally {
+    // Ensuring the client is closed after the operation
     await client.end();
   }
 }
+
